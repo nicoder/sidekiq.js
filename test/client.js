f92e62b9dcb7fe97140472effbc91768c750e036
Mike Perham <mperham@gmail.com>
2016-04-12 16:23:29 -0700

    Clean up code, log redis errors

--- test/client.js
+++ test/client.js
@@ -1,17 +1,23 @@
-'use strict';
+'use strict'
 
-var assert = require("assert");
-var client = require("./helper").client;
+var assert = require('assert')
+var helper = require('./helper')
+var pool = helper.pool
+var client = helper.client
 
-describe('SidekiqClient', function() {
-
-  describe('basic push', function() {
-    it('sends a job to Redis', function(done) {
-      client.push({class: "email", args: [1,2,3]}, function(err, jid) {
-        assert.ok(jid);
-        done(err);
-      });
+describe('SidekiqClient', function () {
+  describe('basic push', function () {
+    it('sends a job to Redis', function (done) {
+      client.push({class: 'email', args: [1, 2, 3]}, function (err, jid) {
+        assert.ok(!err)
+        assert.ok(jid)
+        pool.exec(function (conn) {
+          conn.llen('queue:default', function (err, size) {
+            assert.equal(1, size)
+            done(err)
+          })
+        })
+      })
     })
   })
-
-});
+})
--- test/helper.js
+++ test/helper.js
@@ -1,43 +1,58 @@
-'use strict';
+'use strict'
 
-var Redis = require('ioredis');
+var Redis = require('ioredis')
 var Pool = require('generic-pool').Pool
 
-Pool.prototype.exec = function(fn) {
-  var self = this;
-  self.acquire(function(err, client) {
+Redis.Promise.onPossiblyUnhandledRejection(function (err) {
+  console.error('UNHANDLED REDIS ERROR')
+  console.error(err)
+})
+
+Pool.prototype.exec = function (fn) {
+  var self = this
+  self.acquire(function (err, client) {
     if (err) {
-      throw new Error("Something");
+      throw new Error('Something')
+    } else {
+      fn(client)
+      self.release(client)
     }
-    else {
-      fn(client);
-      self.release(client);
-    }
-  });
+  })
 }
 
-
-// our test database
-var url = "redis://localhost:6379/15";
-
 var pool = new Pool({
-    name     : 'sidekiq.js',
-    create   : function(callback) {
-        callback(null, Redis(url));
-    },
-    destroy  : function(client) { client.disconnect(); },
-    max      : 20,
-    log      : false,
-    idleTimeoutMillis: 15000,
-    reapIntervalMillis: 1000
-});
+  name: 'sidekiq.js',
+  create: function (callback) {
+    var x = new Redis({
+      host: 'localhost',
+      port: 6379,
+      db: 15,
+      connectTimeout: 200,
+      connectionName: 'sidekiq.js',
+      showFriendlyErrorStack: true
+    })
+    x.on('error', function (err) {
+      console.error('REDIS ERROR')
+      console.error(err)
+    })
+    callback(null, x)
+  },
+  destroy: function (client) {
+    client.disconnect()
+  },
+  max: 20,
+  log: false,
+  idleTimeoutMillis: 15000,
+  reapIntervalMillis: 1000
+})
 
 // TODO: Need to make this blocking so the flushdb doesn't happen mid-test.
-pool.exec(conn => {
-  conn.flushdb();
-});
+pool.exec((conn) => {
+  conn.flushdb()
+})
 
-var SidekiqClient = require('../lib/client').SidekiqClient;
-var client = new SidekiqClient(pool);
+var SidekiqClient = require('../lib/client').SidekiqClient
+var client = new SidekiqClient(pool)
 
 exports['client'] = client
+exports['pool'] = pool
