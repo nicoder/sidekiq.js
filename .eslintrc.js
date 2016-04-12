6634387fe22e1b3e5d2798aa74c8cf0e60fde2db
Mike Perham <mperham@gmail.com>
2016-04-12 11:30:36 -0700

    more linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
     "extends": "standard",
     "plugins": [
         "standard"
-    ]
+    ],
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "impliedStrict": true
+      }
+    }
 };
